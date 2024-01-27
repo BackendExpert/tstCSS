36b8488f9a9bdacec7e1dfd12377e7561041a8b6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-27 08:07:12 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- index.php
+++ index.php
@@ -1,40 +1,20 @@
 <?php include("layouts/header.php"); ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>How to Make a Popup</title>
-  <link rel="stylesheet" href="style.css">
-  <script defer src="app.js"></script>
-</head>
-<body>
+<button class="jkbtn jkbtn-blue" id="jkbtnPopup">Click Here</button>
 
-<h2>Animated Modal with Header and Footer</h2>
+<div class="jk-modal" id= "modalJK">
+    <div class="jk-modal-content">
+        <div class="jk-mheader">
+            <h1>JKCSS Modal</h1>
+        <div>
 
+        <div class="jk-mbody">
+            <p>A CSS Framework<p>
+            <p>JKCSS is a one of Framework<p>
+        <div>
 
-<!-- Trigger/Open The Modal -->
-<button id="myBtn">Open Modal</button><br>
-<!-- The Modal -->
-<div id="myModal" class="modal">
-
-  <!-- Modal content -->
-  <div class="modal-content">
-    <div class="modal-header">
-      <span class="close">&times;</span>
-      <h2>Modal Header</h2>
-    </div>
-    <div class="modal-body">
-      <p>Some text in the Modal Body</p>
-      <p>Some other text...</p>
-    </div>
-    <div class="modal-footer">
-      <h3>Modal Footer</h3>
-    </div>
-  </div>
+        <div class="jk-mfooter">
+            <button class="jkbtn jkbtn-green" id="closeModel">Close</button>
+        <div>
+    <div>
 </div>
-
-asdasdasd
-
-</body>
-</html>
