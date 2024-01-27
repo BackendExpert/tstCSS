c1e28fd6f6f13ee0e27ff0221ce4e37151169118
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-27 09:00:37 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
 <?php include("layouts/header.php"); ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Modal Example</title>
+</head>
+<body>
 
-<button class="jkbtn jkbtn-blue">Click Here for Modal</button>
+<button id="openModal">Open Modal</button>
+
+<div id="modal" class="modal">
+  <div class="modal-content">
+    <span class="close">&times;</span>
+    <p>This is a modal!</p>
+  </div>
+</div>
+
+
+<!-- <script src="app.js"></script> -->
+</body>
+</html>
--- layouts/header.php
+++ layouts/header.php
@@ -1,5 +1,3 @@
-<link rel="stylesheet" href="../app.css">
-<script src="../app.css"></script>
+<link rel="stylesheet" href="app.css">
+<script src="app.js"></script>
 
-<link rel="stylesheet" href="https://jkcss-css-framework.github.io/JKCSS-Framework/jkcss/jkcss.min.css">
-<script src="https://jkcss-css-framework.github.io/JKCSS-Framework/js/jkcss.js"></script>
