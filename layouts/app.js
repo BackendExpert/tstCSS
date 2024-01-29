e50af8b86ce04c634229bdbfb756d5f0b675ca1d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-30 04:47:33 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- layouts/app.js
+++ layouts/app.js
@@ -1,10 +1,19 @@
 window.onload = function(){
-  document.getElementById('openModalJK').addEventListener('click', function() {
-    document.getElementById('modalJK').style.display = 'block';
+  var openModals = document.querySelectorAll(".modal-open");
+
+  openModals.forEach(function(btn) {
+    btn.onclick = function() {
+      var modal = btn.getAttribute("data-modal");
+      document.getElementById(modal).style.display = "block";
+    };
   });
   
-  document.getElementsByClassName('close')[0].addEventListener('click', function() {
-    document.getElementById('modalJK').style.display = 'none';
+  var closebtn = document.querySelectorAll('.close-btn');
+  
+  closebtn.forEach(function(btn){
+    btn.onclick = function(){
+      var modal = (btn.closest(".modal").style.display = "none");
+    }
   });
 }
   
