a8d043e69685e182c8abcba076bccb49504c9cb0
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-30 04:48:13 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- index.php
+++ index.php
@@ -10,7 +10,101 @@
 <body>
 
 <div class="site-container">
+<button class="jkbtn jkbtn-blue modal-open" data-modal="modal1">Model
+            1</button>
+        <button class="jkbtn jkbtn-green modal-open" data-modal="modal2">Model
+            2</button>
+        <button class="jkbtn jkbtn-yellow modal-open" data-modal="modal3">Model
+            3</button>
 
+        <div class="modal" id="modal1">
+            <div class="modal-content">
+                <div class="modal-header">
+                    Modal 1
+                    <button class="close-btn">&times;</button>
+                </div>
+                <div class="modal-body">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Tenetur rem est sapiente voluptas repellat nemo temporibus
+                    dolores, quo sed illo dicta quasi et asperiores excepturi
+                    assumenda culpa molestiae pariatur voluptates?
+
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Tenetur rem est sapiente voluptas repellat nemo temporibus
+                    dolores, quo sed illo dicta quasi et asperiores excepturi
+                    assumenda culpa molestiae pariatur voluptates?
+
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Tenetur rem est sapiente voluptas repellat nemo temporibus
+                    dolores, quo sed illo dicta quasi et asperiores excepturi
+                    assumenda culpa molestiae pariatur voluptates?
+                </div>
+                <div class="modal-footer">
+                    <button class="jkbtn jkbtn-blue">Links</button>
+                </div>
+            </div>
+        </div>
+
+        <!-- model 2 -->
+
+        <div class="modal" id="modal2">
+            <div class="modal-content">
+                <div class="modal-header">
+                    Modal 1
+                    <button class="close-btn">&times;</button>
+                </div>
+                <div class="modal-body">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Porro accusamus repudiandae nulla possimus quasi officia eos
+                    consectetur. Quo corrupti eos, maxime vel illo sapiente. Eum
+                    dolorum possimus aspernatur necessitatibus eveniet.
+                    <br>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Porro accusamus repudiandae nulla possimus quasi officia eos
+                    consectetur. Quo corrupti eos, maxime vel illo sapiente. Eum
+                    dolorum possimus aspernatur necessitatibus eveniet.
+                    <br>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Porro accusamus repudiandae nulla possimus quasi officia eos
+                    consectetur. Quo corrupti eos, maxime vel illo sapiente. Eum
+                    dolorum possimus aspernatur necessitatibus eveniet.
+                    <br>
+                </div>
+                <div class="modal-footer">
+                    <button class="jkbtn jkbtn-blue">Links</button>
+                </div>
+            </div>
+        </div>
+
+        <!-- model 3 -->
+
+        <div class="modal" id="modal3">
+            <div class="modal-content">
+                <div class="modal-header">
+                    Modal 1
+                    <button class="close-btn">&times;</button>
+                </div>
+                <div class="modal-body">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Tenetur rem est sapiente voluptas repellat nemo temporibus
+                    dolores, quo sed illo dicta quasi et asperiores excepturi
+                    assumenda culpa molestiae pariatur voluptates?
+
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Tenetur rem est sapiente voluptas repellat nemo temporibus
+                    dolores, quo sed illo dicta quasi et asperiores excepturi
+                    assumenda culpa molestiae pariatur voluptates?
+
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Tenetur rem est sapiente voluptas repellat nemo temporibus
+                    dolores, quo sed illo dicta quasi et asperiores excepturi
+                    assumenda culpa molestiae pariatur voluptates?
+                </div>
+                <div class="modal-footer">
+                    <button class="jkbtn jkbtn-blue">Links</button>
+                </div>
+            </div>
+        </div>
 </div>
 
 <!-- <script src="app.js"></script> -->
