4237abf99e3ee683f003158d829f2228f528a25e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-30 04:58:42 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- layouts/header.php
+++ layouts/header.php
@@ -1,5 +1,5 @@
-<link rel="stylesheet" href="layouts/app.css">
-<script src="layouts/app.js"></script>
+<!-- <link rel="stylesheet" href="layouts/app.css">
+<script src="layouts/app.js"></script> -->
 
 <link rel="stylesheet" href="https://jkcss-css-framework.github.io/JKCSS-Framework/jkcss/jkcss.min.css">
-<!-- <script src="https://jkcss-css-framework.github.io/JKCSS-Framework/js/jkcss.min.js"></script> -->
+<script src="https://jkcss-css-framework.github.io/JKCSS-Framework/js/jkcss.min.js"></script>
